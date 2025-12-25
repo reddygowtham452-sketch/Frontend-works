c7176b1f0bd70ab14eee4827b67e09fb2f59ad9c
gowtham305 <gowtham66305@gmail.com>
2025-12-25 15:16:53 +0530

    Prepare project for push to Frontend-works

--- JAVASCRIPT/Array/array.js
+++ JAVASCRIPT/Array/array.js
@@ -1,76 +1,76 @@
-// // // // !---USING LITERAL WAY ----
-// // // // let arr=["html","css", "javascriprt",1,2];
-// // // // console.log(arr);
-
-// // // let arr=["html", "css"]
-// // // console.log(arr);
-// // // console.log(arr[0]);
-// // // arr[2]="javascript"
-// // // console.log(arr);
-// // // arr[1]="react js"
-// // // console.log(arr);
-// // // delete arr[1]
-// // // console.log(arr);
-// // // console.log(arr[1]);
-
-
-// // // // !--USING ARRAY CONSTRUCTOR----
-// // // let arr1=new Array()
-// // // console.log(arr1);
-// // // arr1[0]=1
-// // // console.log(arr1);
-
-// // // !---USING METHOD OF ARRAYS---
-
-// // // let arr=["html","css","javascript"]
-// // // // console.log(arr.length);          //!--1
-
-// // // // arr.push("bootstrap","reactjs")  //!--2
-// // // // arr.pop()                        //!--3
-// // // // arr.unshift("java", "sql")       //!--4
-// // // // arr.shift()                      //!--5
+// // // !---USING LITERAL WAY ----
+// // // let arr=["html","css", "javascriprt",1,2];
 // // // console.log(arr);
 
-// // // let arr=["html","css","javascript","Bootstrap","React js"]  //!--6
-// // // // // let arr1=arr.slice(0,3)
-// // // // console.log(arr1);  
-
-// // // arr.splice(1,2,"java","sql","phyton","c++")    //!--7
-// // // console.log(arr);
-
-// // let arr=[10,20,30,40,50]
-// // // arr.map((v)=>{
-// // //     console.log(v);
-// //     //    console.log(v+5);
-// // // })
-
-// // let arr1=arr.map((v)=>{
-// //     return v+5;
-// // })
+// // let arr=["html", "css"]
 // // console.log(arr);
+// // console.log(arr[0]);
+// // arr[2]="javascript"
+// // console.log(arr);
+// // arr[1]="react js"
+// // console.log(arr);
+// // delete arr[1]
+// // console.log(arr);
+// // console.log(arr[1]);
+
+
+// // // !--USING ARRAY CONSTRUCTOR----
+// // let arr1=new Array()
+// // console.log(arr1);
+// // arr1[0]=1
 // // console.log(arr1);
 
-// // let arr=[3,5,8,10,11,13]
+// // !---USING METHOD OF ARRAYS---
 
-// // let arr1=arr.filter((a)=>{
-// //     return a%2!=0;
+// // let arr=["html","css","javascript"]
+// // // console.log(arr.length);          //!--1
+
+// // // arr.push("bootstrap","reactjs")  //!--2
+// // // arr.pop()                        //!--3
+// // // arr.unshift("java", "sql")       //!--4
+// // // arr.shift()                      //!--5
+// // console.log(arr);
+
+// // let arr=["html","css","javascript","Bootstrap","React js"]  //!--6
+// // // // let arr1=arr.slice(0,3)
+// // // console.log(arr1);  
+
+// // arr.splice(1,2,"java","sql","phyton","c++")    //!--7
+// // console.log(arr);
+
+// let arr=[10,20,30,40,50]
+// // arr.map((v)=>{
+// //     console.log(v);
+//     //    console.log(v+5);
 // // })
-// // console.log(arr1);
 
-// let arr = [110, 82, 64, 51, 140, 99, 33];
-// // let result = arr
-// //   .filter(v => v > 80)   // keep 110, 82, 140, 99
-// //   .map(v => v + 100);    // add 100 -> 210, 182, 240, 199
+// let arr1=arr.map((v)=>{
+//     return v+5;
+// })
+// console.log(arr);
+// console.log(arr1);
 
-// // console.log(result); // [210, 182, 240, 199]
+// let arr=[3,5,8,10,11,13]
 
-// // let arr1=arr.filter((v)=>{
-// //     return v>80
-// // })
-// // console.log(arr1);
+// let arr1=arr.filter((a)=>{
+//     return a%2!=0;
+// })
+// console.log(arr1);
 
-// // let arr2=arr1.map((v)=>{
-// //     return v+100
-// // })
-// // console.log(arr2);
+let arr = [110, 82, 64, 51, 140, 99, 33];
+// let result = arr
+//   .filter(v => v > 80)   // keep 110, 82, 140, 99
+//   .map(v => v + 100);    // add 100 -> 210, 182, 240, 199
+
+// console.log(result); // [210, 182, 240, 199]
+
+// let arr1=arr.filter((v)=>{
+//     return v>80
+// })
+// console.log(arr1);
+
+// let arr2=arr1.map((v)=>{
+//     return v+100
+// })
+// console.log(arr2);
 
